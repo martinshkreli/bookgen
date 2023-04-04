9ba55b6f64e87c9fd2db0935a13fd5c354bb276b
Martin Shkreli <shkrelimartin@gmail.com>
2023-04-04 01:14:29 -0400

    version 2.0!

--- app.js
+++ app.js
@@ -1,79 +1,201 @@
-import { Configuration, OpenAIApi } from "openai";
-import request from 'request';
-import fs, { write } from 'fs';
-const writeStream = fs.createWriteStream('story.txt');
-const configuration = new Configuration({
-    organization: "org-",
-    apiKey: "sk-",
-});
-const openai = new OpenAIApi(configuration);
-const response = await openai.listEngines();
+require('dotenv').config();
+const readline = require('readline');
+const request = require("request");
+const fs = require('fs');
+const { resolve } = require('path');
+const { askOpenAI } = require("./askOpenAI");
 
-async function runIt(parameter) {
-    let prompt = `${parameter}`;
+function countWords(str) {
+  return str.trim().split(/\s+/).length;
+}
 
-    let payload = {
-        'method': 'POST',
-        'url': 'https://api.openai.com/v1/completions',
-        'headers': {
-        'Content-Type': 'application/json',
-        'Authorization': "Bearer sk-",
-        },
-        'body': JSON.stringify({
-            'model': 'text-davinci-003',
-        "prompt": prompt,
-        "max_tokens": 4000,
-        "temperature": 0.8,
-        })
-    };
-    return new Promise(function (resolve, reject) {
-        request(payload, (error, response, body) => {
-            if (error) {
-                console.log(error);
-                reject(error);
-            } else {
-                let body = JSON.parse(response.body);
-                resolve(response);
-            }
+let models = {
+  'gpt35': {
+    name: 'gpt-3.5-turbo',
+    tokenLimit: 4097,
+  },
+  'gpt4': {
+    name: 'gpt-4-0314',
+    tokenLimit: 8000,
+  } 
+}
+
+let padAmount = 300;
+let chapterLength = 20;
+
+let genresList = ['Action', 'Adventure', 'Alternative History', 'Apocalyptic', 'Children\'s', 'Comedy', 'Crime', 'Cyberpunk', 'Drama', 'Dystopian', 'Elizabethan', 'Existentialist', 'Fantasy', 'Gothic', 'Historical', 'Horror', 'Legal', 'LGBTQ', 'Magical Realism', 'Mystery', 'Near-future', 'Parable', 'Paranormal', 'Post-Apocalyptic', 'Romance', 'Postmodern', 'Science Fiction', 'Sports', 'Star Trek Fan Fiction', 'Steampunk', 'Superhero', 'Supernatural', 'Teenage', 'Thriller', 'Tragedy', 'Utopian', 'Victorian', 'Western', 'Young Adult'];
+
+let randomGenre = genresList[Math.floor(Math.random() * genresList.length)];
+console.clear();
+readline.cursorTo(process.stdout,1,1)
+process.stdout.write(`\x1b[34mGenre is: ${randomGenre}`);
+
+let state ={
+  manuscript: [],  
+  desiredPages: 200,
+  chapters: 0,
+  chapterText: [],
+  chapterSummary: [],
+  chapterSummaryLong: [],
+  manuscriptSummaryToDate: '',
+  plotOutline: '',
+  plotGenre: randomGenre,
+  mainCharacters: [],
+  minorCharacters: [],
+  plotSettings: [],  
+  writingStyle: '',
+  writingAdjectives: '',
+  rawOutline: '',
+  fullText: []
+}
+
+state.chapters = Math.floor(state.desiredPages / chapterLength);
+
+async function outlineGenerator(state){
+  let outlinePrompt =  `Generate the outline of an original ${state.desiredpages}-page ${state.plotGenre} fiction book. Imagine and then carefully label the following: a plot, characters with names, settings and writing style. You have ${8000 - (40 + padAmount)} tokens remaining to write the outline. Use as many as possible.`;
+
+  readline.cursorTo(process.stdout,2,2)
+  process.stdout.write(`\x1b[35mGenerating Outline:`); 
+  
+  let outline = await askOpenAI(outlinePrompt, 'writer', 'gpt-4-0314', (8000 - (40 + padAmount)), 0.8)
+  outline = outline.choices[0].message.content;
+  
+  //  let templot = outline.replace(/\n/g, '');
+  state.rawOutline = outline;
+  readline.cursorTo(process.stdout,3,5);
+  process.stdout.write(`\x1b[35mhere is the raw outline`);
+  readline.cursorTo(process.stdout,4,6);
+  process.stdout.write(`\x1b[34m${outline}`);
+}
+
+async function statePopulator(state){
+  await outlineGenerator(state);
+  
+  console.log('\npopulating state from raw outline')
+  let itemsToPopulateHashMap = {
+    'plotOutline': 'plot',
+    'mainCharacters': 'main characters',
+    'minorCharacters': 'minor characters',
+    'plotSettings': 'setting',
+    'writingStyle': 'writing style',
+    'writingAdjectives': 'writing adjectives'
+  }
+
+  for (const [key, keyVal] of Object.entries(itemsToPopulateHashMap)){
+    let summaryToCount = `I'm going to give you the outline of a book. From this outline, tell me the ${keyVal}. You have 1234 tokens (or words) left for this page. Here is the outline: ${state.rawOutline}`;
+    
+    let summaryLength = countWords(summaryToCount);
+    
+    console.log(summaryLength)
+    
+    let statePopulatorPrompt = `I'm going to give you the outline of a book. From this outline, tell me the ${keyVal}. Use close to ${4097 - (summaryLength + padAmount)} words for this page. Here is the outline: ${state.rawOutline}`;
+
+    let statePopulatorResult = await askOpenAI(statePopulatorPrompt, 'machine', 'gpt-4-0314', (8000 - (summaryLength + padAmount)), 0.8)
+
+    statePopulatorResult = statePopulatorResult.choices[0].message.content;
+
+    state[key] = statePopulatorResult;
+    process.stdout.write(`\n\x1b[33m here is the ${keyVal}: ${statePopulatorResult}\n`);
+  }
+  
+  process.stdout.write(`\n\x1b[34mhere is the state object:\n`)
+  console.log(state);
+
+  console.log('generating one sentence summaries');
+  let stringToCount = `You are writing a book with this plot summary: ${state.plotOutline}. The book is ${state.desiredPages} pages long. Write a specific one-page plot summary for each of the ten chapters of the book. Name any unnamed major or minor characters. Use the first few chapters to introduce the characters and set the story. Use the next few chapters for action and character development. Use the last few chapters for dramatic twists in the plot and conclusion. You have 1234 tokens (or words) left for the summaries.`
+  let stringLength = countWords(stringToCount);
+  console.log(stringLength);
+
+  let chapterSummaryText = await askOpenAI(`You are writing a book with this plot summary: ${state.plotOutline}. The book is ${state.desiredPages} pages long. Write a specific one-page plot summary for each of the ten chapters of the book. Name any unnamed major or minor characters. Use the first few chapters to introduce the characters and set the story. Use the next few chapters for action and character development. Use the last few chapters for dramatic twists in the plot and conclusion. You have ${8000 - (stringLength + padAmount)} tokens (or words) left for the summaries.`, 'writer', 'gpt-4-0314', (8000 - (stringLength + padAmount)), 0.8)
+  
+  chapterSummaryText = chapterSummaryText.choices[0].message.content;
+  
+  chapterSummaryText = chapterSummaryText.split(/\n/).filter((x) => x.length > 5);
+  process.stdout.write(`\x1b[34mChapter Summary Text`)
+  state.chapterSummary = chapterSummaryText;
+
+  for (var i=0; i<state.chapters; i++){
+    process.stdout.write(`\x1b[36mGenerating short summaries`)
+    console.log('Chapter ', i+1);
+    console.log(`asking chat gpt to write a summary of ${state.chapterSummary[i]}`)
+
+    let shortSummaryPrompt = `You are writing Chapter ${i+1} of a ${state.chapters} chapter ${state.plotGenre} book. The plot summary for the entire book is ${state.plotOutline}. The summary of this chapter is ${state.chapterSummary[i]}. Write a several paragraph summary of the chapter. Add new subplots, character development, character background, planned dialogue and plot development that you would typically find in such a work. You have 1234 tokens (or words) left for this.}`
+
+    let shortSummaryLength = countWords(shortSummaryPrompt);
+
+    console.log(shortSummaryLength)
+    try {
+      let shortSummaryText = ''
+      while (shortSummaryText.length < 100) {
+        shortSummaryText = await askOpenAI(`You are writing Chapter ${i+1} of a ${state.chapters} chapter ${state.plotGenre} book. The plot summary for the entire book is ${state.plotOutline}. The summary of this chapter is ${state.chapterSummary[i]}. Write a 4-page summary of the chapter. Add new subplots, character development, character background, planned dialogue and plot development that you would typically find in such a work. You have ${8000 - (shortSummaryLength + padAmount)} tokens (or words) left for this.}`, 'writer', 'gpt-4-0314', (8000 - (shortSummaryLength + padAmount)), 0.8)
+
+        if (!shortSummaryText.choices[0]) {shortSummaryText = 'error'}
+        if (!shortSummaryText.choices[0].message) {shortSummaryText = 'error'}
+        if (!shortSummaryText.choices[0].message.content) {
+          shortSummaryText = 'error'
+        } else {
+          shortSummaryText = shortSummaryText.choices[0].message.content;
+          shortSummaryText = shortSummaryText.replace(/\n/g, '');
+          state.chapterSummaryLong.push(shortSummaryText);
+          process.stdout.write(`\r\x1b[35mHere is the chapter summary: ${state.chapterSummaryLong[i]}`);
+        }        
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  console.log('entering page generation module');
+
+  for (var i=0; i<state.chapters; i++){
+    for (var j=0; j<20; j++) {
+      let amendment = ""
+      if (j==0) {amendment = "";}
+      if (j !==0 ) {amendment = `Page ${j} reads as follows ${state.fullText[(i*j + j)]} `;}
+      console.log('generating final full text for chapter ', i+1, ' page ', j+1);
+
+      let pageGenPrompt = `You are a serious author writing page ${j+1} in chapter ${i+1} of a ${state.chapters}-chapter ${state.plotGenre} novel. The plot summary for this chapter is ${state.chapterSummaryLong[i]}. ${amendment} As you are writing, be sure to develop the characters background thoroughly, include dialogue and detailed literary descriptions of the scenery, and develop the plot. Do not mention page or chapter numbers! Do not jump to the end of the plot. The prior page reads as follows: You have 1234 tokens (or words) left for this page.`
+
+      let pageGenLength = countWords(pageGenPrompt);
+      let pageGenText = ''
+      try {
+        while (pageGenText.length < 100) {
+          let pageGenText = await askOpenAI(`You are a serious author writing page ${j+1} in chapter ${i+1} of a ${state.chapters}-chapter ${state.plotGenre} novel. The plot summary for this chapter is ${state.chapterSummaryLong[i]}. ${amendment} As you are writing, be sure to develop the characters background thoroughly, include dialogue and detailed literary descriptions of the scenery, and develop the plot. Do not mention page or chapter numbers! Do not jump to the end of the plot. The prior page reads as follows: You have ${8000 - (pageGenLength + padAmount)} tokens (or words) left for this page.`, 'writer', 'gpt-4-0314', 8000 - (pageGenLength + padAmount), 0.8)
+          
+          pageGenText = pageGenText.choices[0].message.content;
+          pageGenText = pageGenText.replace(/\n/g, '');
+          state.fullText.push((pageGenText + "\n"));
+        }
+      } catch (err) {
+        console.log(err);
+      }
+        fs.appendFile("story.txt", pageGenText, (err) => {
+          if (err) {
+            throw err;
+          }
         });
+          process.stdout.write(`\x1b[35mhere is the page text: ${pageGenText}`);
+      }
+
+    let chapterheader = `\n\nChapter ${i+1}\n\n`
+    fs.appendFile("story.txt", chapterheader, (err) => {
+      if (err) {
+        throw err;
+      }
     });
+  }
 }
+statePopulator(state);
 
-function singleQuestion(question) {
-  let prompt = `${question}`;
-  return runIt(prompt);
-}
-let story = [
-    "Quest stood there, a picture of stillness and grace, his hands tucked into the pockets of his well-worn trousers. The sun shone down on him, painting his skin a golden hue, and his eyes glinted with a deep intelligence. He could feel the breeze rippling through his hair, and he breathed in the salty air. He was at peace in that moment, and the world seemed to stand still around him. He knew that his life was about to change, and he welcomed it with open arms.",
-    "He was not motionless for long; his body soon stirred, and he began to walk slowly towards the shoreline. The sand was warm beneath his feet, betraying a heat that was not yet visible in the air, and he savoured the sensation as he walked. He felt a new energy coursing through his veins, and the anticipation of what lay ahead was electrifying. As he drew nearer the horizon, he could make out the distant shapes of fishing boats and the figures of seagulls wheeling in the sky. The ocean seemed to beckon him, and he felt himself drawn ever closer, as if pulled by an unseen force. He had no idea what awaited him, but he was sure of one thing – his life was about to take an unexpected turn.",
-    "The salty smell of the sea filled his nostrils, and he was reminded of a time long past - a time when his life was lived in a different way, before the monotony of the modern day had dulled his senses. He continued towards the shoreline, eager to discover what was waiting for him. As he drew nearer, the sound of crashing waves and the cries of the seagulls grew louder, and the excitement of the unknown began to take hold. He could see the boats bobbing up and down in the water, and he felt an emotion he had not felt in a long time - hope. He was suddenly struck by the thought of setting sail on one of them, of travelling to some distant and undiscovered land, of exploring and of adventure. He picked up his pace, and soon the ocean was before him, vast and inviting. He stood, taking in the sight before him and marveling at the power of the sea. He felt a strange urge to jump in and let the waves carry him away, but he held back and instead took a deep breath as he stepped forward, his feet sinking into the sand, and all the while sensing that his life was about to take an unexpected turn.",
-    "The sun blazed brightly in the sky, yet its warmth was a distant memory to the man as he felt the cold Atlantic spray wash against his skin. He could taste the salt, feel the power of the water as it thundered against the shore. He had seen enough of the sea in his life to know what was needed here: a vessel. He scanned the shore, hoping to find his salvation, and soon his gaze alighted on a weathered fishing boat, far from the shore but still visible in the depths of the bay. He set off, determined, and soon enough the boat was close enough for him to make out its details: it was small, but would do, with a single mast and a yellow sail. He clambered aboard, feeling the familiarity of the vessel that had once been his home. His journey was about to begin. With a few confident thrusts of the oar, he set off, letting the current take him as he set his sights on the horizon and the distant shores beyond. As the miles rolled by, the man felt a strange sense of anticipation - of what, he could not tell, but it was a feeling of longing, of hope. He had found the boat and a purpose, and that was enough. He was free, and his destination was open to him. With a smile, he allowed himself to be taken away by the wind and waves, his journey just beginning."
-];
-let storyOutline = [
-    "The sun shone down on Quest, casting a golden hue on his skin, and his eyes glinted with intelligence as he stood there in peace, knowing his life was about to change.",
-    "He was drawn to the shoreline, where he met a woman named Emmy, feeling a new energy as the anticipation of the unknown spurred him onward.",
-    "The sun shone brightly in the sky and a gentle breeze blew through the trees, creating a peaceful atmosphere. He thought about his old friend David.",
-    "The man found a boat to sail on that would take him on a journey with a feeling of anticipation and hope. On the journey he met the notorious troll named Ikk."
-]
-
-for (var i=0; i<250; i++){
-  await singleQuestion(`Here are four one-sentence summaries of four paragraphs of a story. 1: ${storyOutline[i]} 2: ${storyOutline[i+1]} 3: ${storyOutline[i+2]} 4: ${storyOutline[i+3]}. Write a fifth fictional paragraph in the writing style of James Patterson that progresses the plot of the story. Vary the plot from the first four paragraphs. Make sure there is action, dialogue and do not be repetitive by starting each paragraph the words "The man".`).then((x) => {
-    let response = JSON.parse(x.body);
-    let tempans = response.choices[0].text;
-    tempans = tempans.replace(/\n/g, '');
-    console.log('Here is the new paragraph: ', tempans);
-    story.push(tempans)});
-  await singleQuestion(`Summarize this paragraph in one sentence: ${story[i+4]}`).then((x) => {
-    let response = JSON.parse(x.body);
-    let tempans = response.choices[0].text;
-    tempans = tempans.replace(/\n/g, '');
-    console.log('Here is the summary of that paragraph: ', tempans);
-    storyOutline.push(tempans)
-  });
-}
-
-story.forEach(value => writeStream.write(`${value}\n`));
-writeStream.on('finish', () => {
-  console.log('done writing file');
-})
-writeStream.end();
+//1. Identify what the main theme or plot of the novel is going to be
+//2. Brainstorm ideas for the main characters, their traits, and struggles.
+//3. Create a timeline for the novel, including significant events and developments that will take place.
+//4. Develop a setting for the novel, either real or imaginary.
+//5. Establish the main characters and their motivations.
+//6. Introduce the initial conflict and how it will be solved.
+//7. Develop sub-plots for the novel using the characters and their struggles.
+//8. Introduce necessary complications, such as other characters, that will further the story.
+//9. Describe the environments, scenery, and settings that the characters will interact with
+//10. Develop the climax of the novel and resolve the conflict
+//11. Describe the resolution of the novel, explaining how it all wraps up
+//12. Run the novel through Open AI and have it generate successive, non-repetitive chapters based on the initial input and timeline
---
+++ askOpenAI.js
@@ -0,0 +1,46 @@
+const askOpenAI = async function (prompt, role, modelChoice = 'gpt-3.5-turbo', tokens=5000, temp=0.65) {
+    let now = new Date();
+    // let model = 'gpt-3.5-turbo' // //gpt-4-0314
+    let roleContent = "You are an ChatGPT-powered chat bot."
+
+    if (role == 'machine') {
+        roleContent = "You are a computer program attempting to comply with the user's wishes."
+    }
+
+    if (role == 'writer') {
+        roleContent = "You are a professional fiction writer who is a best-selling author. You use all of the rhetorical devices you know to write a compelling book."
+    }
+
+    return new Promise(function(resolve, reject) {
+        fetch('https://api.openai.com/v1/chat/completions', {
+            method: 'POST',
+            mode: 'cors',
+            cache: 'no-cache',
+            credentials: 'same-origin',
+            'headers': {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${process.env.API_KEY}`,
+            },
+            'body': JSON.stringify({
+                'model': modelChoice,
+                "messages": [
+                    {"role": "system", "content": roleContent},
+                    {"role": "user", "content": prompt},
+                ],
+                "max_tokens": tokens,
+                "temperature": temp,
+            })
+        }).then(response => response.json()).then(data => {
+            try {
+                let elapsed = new Date() - now;
+                console.log('\nOpenAI response time: ' + elapsed + 'ms\n')
+                resolve (data)
+            } catch(e) {
+                console.log(e);
+                resolve (e);
+            }
+        })
+    });
+}
+
+exports.askOpenAI = askOpenAI;
